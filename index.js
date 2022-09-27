0e0db744d498baf13d34ccf25d1b0cd15f3d1923
Abhishek gupta <abhishek.pec2015@gmail.com>
2022-09-28 01:23:21 +0530

    ball flying

--- index.js
+++ index.js
@@ -3,18 +3,34 @@ const keys = {
   RIGHT: "ArrowRight",
 };
 
+const direction = {
+  BOTTOM_RIGHT: "bottomRight",
+  BOTTOM_LEFT: "bottomLeft",
+  TOP_LEFT: "topLeft",
+  TOP_RIGHT: "topRight",
+};
+
+const BALL_STEP = 1;
+
 //Returns bar position in percentage
-const getBarPosition = (bar) => {
+const getBarPositionX = (bar) => {
   const screenWidth = window.outerWidth;
   let left = Number(bar.css("left").replace("px", ""));
   return (left * 100) / screenWidth;
+};
+
+//Returns bar position in percentage
+const getBarPositionY = (bar) => {
+  let screenHeigth = $("#ballArea").height();
+  let top = Number(bar.css("top").replace("px", ""));
+  return (top * 100) / screenHeigth;
 };
 
 (function () {
   var goToRight = false;
   setInterval(() => {
     const bar = $("#computerBar");
-    let barPosition = getBarPosition(bar);
+    let barPosition = getBarPositionX(bar);
     if (barPosition == 0) goToRight = true;
     if (barPosition == 80) goToRight = false;
     if (goToRight) barPosition += 0.2;
@@ -25,17 +41,88 @@ const getBarPosition = (bar) => {
   }, 10);
 })();
 
+(function () {
+  var goToDirection = direction.BOTTOM_RIGHT;
+  setInterval(() => {
+    const ball = $("#ball");
+    const ballAreaHeight = parseInt($("#ballArea").height());
+    const ballAreaWidth = parseInt($("#ballArea").width())-30;
+    let left = parseInt(Number(ball.css("left").replace("px", "")));
+    let top =  parseInt(Number(ball.css("top").replace("px", "")));
+
+    if(top === ballAreaHeight){
+      if(goToDirection === direction.BOTTOM_RIGHT){
+        goToDirection = direction.TOP_RIGHT;
+      }
+      else{
+        goToDirection = direction.TOP_LEFT;
+      }
+    }
+    if(top === 0){
+      if(goToDirection === direction.TOP_RIGHT){
+        goToDirection = direction.BOTTOM_RIGHT;
+      }else{
+        goToDirection = direction.BOTTOM_LEFT;
+      }
+    }
+
+    if(left === ballAreaWidth){
+      if(goToDirection === direction.TOP_RIGHT){
+        goToDirection = direction.TOP_LEFT;
+      }else{
+        goToDirection = direction.BOTTOM_LEFT;
+      }
+    }
+
+    if(left === 0){
+      if(goToDirection === direction.TOP_LEFT){
+        goToDirection = direction.TOP_RIGHT;
+      }else{
+        goToDirection = direction.BOTTOM_RIGHT;
+      }
+    }
+    
+
+    if (goToDirection === direction.BOTTOM_RIGHT) {
+      left += BALL_STEP;
+      left += "px";
+      top += BALL_STEP;
+      top += "px";
+    }
+    if (goToDirection === direction.BOTTOM_LEFT) {
+      left -= BALL_STEP;
+      left += "px";
+      top += BALL_STEP;
+      top += "px";
+    }
+    if (goToDirection === direction.TOP_LEFT) {
+      left -= BALL_STEP;
+      left += "px";
+      top -= BALL_STEP;
+      top += "px";
+    }
+    if (goToDirection === direction.TOP_RIGHT) {
+      left += BALL_STEP;
+      left += "px";
+      top -= BALL_STEP;
+      top += "px";
+    }
+    ball.css("left", left);
+    ball.css("top", top);
+  }, 5);
+})();
+
 $("body").on("keydown", (event) => {
   // if
   const key = event.originalEvent.key;
   if (key === keys.LEFT || key === keys.RIGHT) {
     const bar = $("#playerBar");
-    let barPosition = getBarPosition(bar);
+    let barPosition = getBarPositionX(bar);
     if (key === keys.LEFT && barPosition >= 4) {
-        barPosition = barPosition - 4;
+      barPosition = barPosition - 4;
     }
     if (key === keys.RIGHT && barPosition <= 77) {
-        barPosition = barPosition + 4;
+      barPosition = barPosition + 4;
     }
     barPosition = barPosition + "vw";
     bar.css("left", barPosition);
